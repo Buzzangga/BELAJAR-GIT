762dbf62d10980a34e02e9998095f8307d3260d5
HAHAHAHA <ah3545987@gmail.com>
2026-09-19 08:47:33 +0700

    Menambahkan perubahan pada dev-Angga

--- angga.dart
+++ angga.dart
@@ -1,11 +1,13 @@
-void main () {
+void main() {
   int decimal = 10;
-int octal = int.parse('12', radix: 8);
-int binary = int.parse('1010', radix: 2);
-int hexadecimal = 0xA;
+  int octal = int.parse('12', radix: 8);
+  int binary = int.parse('1010', radix: 2);
+  int hexadecimal = 0xA;
 
-print(decimal);
-print(binary);
-print(hexadecimal);
-print(octal);
+  print(decimal);
+  print(binary);
+  print(hexadecimal);
+  print(octal);
+
+  print("Perubahan pada branch dev-Angga");
 }
